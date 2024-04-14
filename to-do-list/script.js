62711dba1e735f5bf86181539803185f7ba27d61
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-04-13 21:23:21 -0700

    Seems to be working

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,4 +1,5 @@
 var latest_task_num = 0;
+const TEMPLATE_DIV = document.querySelector('#tasks-div > .container.task:first-child');
 
 function move_task(task_div, up) {
     let parent_div = task_div.parentNode;
@@ -30,38 +31,44 @@ function process_button_activation() {
     };
 };
 
-
 function add_task() {
     const add_task_input = document.getElementById("add-task-input");
     if (!add_task_input.value) {
         window.alert("No task title added");
         return false;
     }
-    let task_div;
-    const original_div = document.getElementsByClassName("task")[0];
-    if (latest_task_num == 0) {
-        task_div = original_div;
-    } else {
-        // Clone the original div
-        task_div = original_div.cloneNode(true);
-        // Insert the cloned div after the original div
-        original_div.parentNode.insertBefore(task_div, original_div.nextSibling);
-    }
+    // Clone the first div
+    let task_div = TEMPLATE_DIV.cloneNode(true);
+    // Insert the cloned div after the first div
+    TEMPLATE_DIV.parentNode.insertAdjacentElement('afterend', task_div);
     latest_task_num++;
-    // Assign id and "click" event
     task_div.id = `task-idem-div-${latest_task_num}`;
+    const task_checkbox = task_div.querySelector("input");
+    const task_label = task_div.querySelector("label");
+    const remove_button = task_div.getElementsByClassName("remove-button")[0];
     const up_button = task_div.getElementsByClassName("up-button")[0];
+    const down_button = task_div.getElementsByClassName("down-button")[0];
+    task_checkbox.id = `task-checkbox${latest_task_num}`;
+    task_label.setAttribute('for', task_checkbox.id);
+    task_label.classList.remove('strikethrough');
+    remove_button.addEventListener("click", () => {task_div.remove()});
+    task_checkbox.addEventListener('change', function() {
+        if (this.checked) {
+          task_label.classList.add('strikethrough');
+        } else {
+          task_label.classList.remove('strikethrough');
+        }
+      });
     up_button.addEventListener("click", () => {
         move_task(task_div, true)
     });
-    const down_button = task_div.getElementsByClassName("down-button")[0];
     down_button.addEventListener("click", () => {
         move_task(task_div, false)
     });
     task_div.style.display = 'block'; // Make the container visible
-    const task_label = document.getElementById("task-label");
     task_label.textContent = add_task_input.value;
     add_task_input.value = null;
+    task_checkbox.checked = false;
     process_button_activation();
     return true;
 }
